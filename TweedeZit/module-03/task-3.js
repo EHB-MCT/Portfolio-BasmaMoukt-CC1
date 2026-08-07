87754f5dc9173023f7984e84c06c49d19c2854d3
BasmaMoukt <basma.mouktaoui.erramah@student.ehb.be>
2026-08-07 22:54:47 +0200

    linker helft functie

--- TweedeZit/module-03/task-3.js
+++ TweedeZit/module-03/task-3.js
@@ -3,3 +3,13 @@ function setup() {
 	background("white");
 	noStroke();
 }
+
+//linker kant
+function draw() {
+	for (let i = 0; i < 10; i++) {
+		let x = random(height / 2);
+		let y = random(width / 2);
+		(random(height * 0.06, height * 0.1),
+			color(random(40, 120), random(80, 160), random(160, 220)));
+	}
+}
