83aa424f59c04a065e8c4fe8eb9fc5bb1f485e33
BasmaMoukt <basma.mouktaoui.erramah@student.ehb.be>
2026-08-15 20:24:23 +0200

    functions

--- sketches/exam3.js
+++ sketches/exam3.js
@@ -1,18 +1,23 @@
-createCanvas(600, 400);
-background("lightblue");
-noFill();
-noStroke();
-
-let xS = width / 2;
-let yS = height / 2;
-fill("yellow");
-circle(xS, 200, yS - 90); //zon
+function setup() {
+	createCanvas(600, 400);
+	background("lightblue");
+	noFill();
+	noStroke();
+}
 
 let x = random(300);
 let y = random(300);
 
-fill(120);
-circle(x, y, 100); //midden
-circle(x - 50, y + 50 - 30, 60); //links
-rect(x - 50, y, 100, 50);
-circle(x + 50, y + 50 - 35, 70); //rechts
+function draw() {
+	let xS = width / 2;
+	let yS = height / 2;
+
+	fill("yellow");
+	circle(xS, 200, yS - 90); //zon
+
+	fill(120);
+	circle(x, y, 100); //midden
+	circle(x - 50, y + 50 - 30, 60); //links
+	rect(x - 50, y, 100, 50);
+	circle(x + 50, y + 50 - 35, 70); //rechts
+}
