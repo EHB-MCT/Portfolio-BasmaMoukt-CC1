8bd1ae49a1b3112164a8656d4431ea11fb1f36c5
BasmaMoukt <basma.mouktaoui.erramah@student.ehb.be>
2026-08-15 21:03:32 +0200

    random blokken positie

--- sketches/exam3.js
+++ sketches/exam3.js
@@ -5,8 +5,8 @@ function setup() {
 	noStroke();
 }
 
-let x = random(300);
-let y = random(300);
+let x = random(600);
+let y = random(400);
 
 function draw() {
 	let xS = width / 2;
@@ -20,4 +20,28 @@ function draw() {
 	circle(x - 50, y + 50 - 30, 60); //links
 	rect(x - 50, y, 100, 50);
 	circle(x + 50, y + 50 - 35, 70); //rechts
+
+	fill("red");
+	let xR = random(600);
+	let yR = random(400);
+	rect(x, y, 100);
+	noLoop();
+
+	fill("blue");
+	let xT = random(600);
+	let yT = random(400);
+	rect(xT, yT, 120);
+	noLoop();
+
+	fill("green");
+	let xB = random(600);
+	let yB = random(400);
+	rect(xB, yB, 90);
+	noLoop();
+
+	fill("orange");
+	let xO = random(600);
+	let yO = random(400);
+	rect(xO, yO, 50);
+	noLoop();
 }
