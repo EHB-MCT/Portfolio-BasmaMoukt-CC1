b2f448a06fa728164007044c1f14bae42d92331d
BasmaMoukt <basma.mouktaoui.erramah@student.ehb.be>
2026-08-15 20:11:25 +0200

    sun + random position clouds

--- sketches/exam3.js
+++ sketches/exam3.js
@@ -1,15 +1,18 @@
-createCanvas(400, 600);
-background(255);
+createCanvas(600, 400);
+background("lightblue");
 noFill();
 noStroke();
 
-let x = width / 2;
-let y = height / 2;
+let xS = width / 2;
+let yS = height / 2;
+fill("yellow");
+circle(xS, 200, yS - 90); //zon
+
+let x = random(300);
+let y = random(300);
 
 fill(120);
 circle(x, y, 100); //midden
 circle(x - 50, y + 50 - 30, 60); //links
 rect(x - 50, y, 100, 50);
 circle(x + 50, y + 50 - 35, 70); //rechts
-
-let;
