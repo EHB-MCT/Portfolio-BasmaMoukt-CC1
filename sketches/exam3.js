94c5ef2a817a3acd195b2f658702d273ab8218ee
BasmaMoukt <basma.mouktaoui.erramah@student.ehb.be>
2026-08-15 20:00:02 +0200

    wolk

---
+++ sketches/exam3.js
@@ -0,0 +1,15 @@
+createCanvas(400, 600);
+background(255);
+noFill();
+noStroke();
+
+let x = width / 2;
+let y = height / 2;
+
+fill(120);
+circle(x, y, 100); //midden
+circle(x - 50, y + 50 - 30, 60); //links
+rect(x - 50, y, 100, 50);
+circle(x + 50, y + 50 - 35, 70); //rechts
+
+let;
